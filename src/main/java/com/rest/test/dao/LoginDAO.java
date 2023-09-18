024dac3d2c64c672ef0ff3925bee001586a99ea5
stoneone098 <Kabil@DESKTOP-K49NN5G>
2023-09-18 12:21:15 +0530

    setup

--- src/main/java/com/rest/test/dao/LoginDAO.java
+++ src/main/java/com/rest/test/dao/LoginDAO.java
@@ -1,9 +1,10 @@
 package com.rest.test.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.rest.test.pojo.LoginVO;
-
+@Service
 public class LoginDAO {
 	@Autowired
 	
